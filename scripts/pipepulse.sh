1ceed6e46484cd1a41efa312a26f3b370250e47e
controlshifted <gabriel.montplaisir@protonmail.com>
2022-05-04 22:59:58 -0400

    Removed Jack_Mixer

--- scripts/pipepulse.sh
+++ scripts/pipepulse.sh
@@ -1,5 +1,4 @@
-jack_mixer -c ~/.config/jack_mixer/config.xml &
 pactl load-module module-null-sink media.class=Audio/Sink sink_name=pipe_sink
 pactl load-module module-null-sink media.class=Audio/Sink sink_name=WebMusic_OUT channel_map=stereo
-pactl load-module module-null-sink media.class=Audio/Duplex sink_name=DISCORD_IN_OUT channel_map=stereo
+pactl load-module module-null-sink media.class=Audio/Sink sink_name=Discord_OUT channel_map=stereo
 pactl set-default-sink pipe_sink
